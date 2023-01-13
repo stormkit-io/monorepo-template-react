01704b5f2e87565c48179b9e5ea8455f0e1f44ab
Savas Vedova <savas@stormkit.io>
2023-01-13 20:26:47 +0100

    feat: build for ssr

---
+++ vite.config.ssr.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import pj from "./package.json";
+
+process.env.NODE_ENV = "production";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  ssr: {
+    noExternal: Object.keys(pj.dependencies || {}).concat(
+      Object.keys(pj.devDependencies || {})
+    ),
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^~/,
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  build: {
+    ssr: true,
+    minify: false,
+    rollupOptions: {
+      preserveEntrySignatures: "strict",
+      input: { server: "src/entry-server.tsx" },
+      output: {
+        dir: ".stormkit/server",
+        format: "esm",
+        entryFileNames: "[name].mjs",
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        exports: "named",
+      },
+    },
+  },
+  plugins: [react()],
+});
