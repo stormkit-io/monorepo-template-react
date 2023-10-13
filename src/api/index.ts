307382bb9363263102d313ee6b03af4f11ec8d13
Savas Vedova <savas@stormkit.io>
2023-10-13 23:22:23 +0200

    chore: use alternative syntax

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
-import type { IncomingMessage, ServerResponse } from "node:http";
+import type { IncomingMessage } from "node:http";
 
-export default (_: IncomingMessage, res: ServerResponse) => {
-  res.setHeader("Content-Type", "application/json");
-  res.writeHead(200, "Success");
-  res.write(JSON.stringify({ status: "running" }));
-  res.end();
+export default (_: IncomingMessage) => {
+  return {
+    body: { status: "running" },
+    status: 200,
+  };
 };
